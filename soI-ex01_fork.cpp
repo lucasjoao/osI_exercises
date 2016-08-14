0fdf402c08e1681674770f2fc740b698e0d90728
lucasjoao <lucasjoao.lj@gmail.com>
2016-08-14 20:11:10 -0300

    add doc and change order of final thing

--- soI-ex01_fork.cpp
+++ soI-ex01_fork.cpp
@@ -6,15 +6,43 @@
 #include <sys/wait.h>
 // nome do acadêmico: lucas joao martins
 
+/**
+*@brief   linux system calls and api based on unit "Foundamentals of Operating
+          Systems"
+*@details atividade 1 da disciplina ine5412. Um processo cria cinco processos
+          filhos, quando todos os filhos acabam o pai retorna a soma dos
+          status que cada filho retornou.
+*@return  status do fim da execucao do programa
+**/
+
 using namespace std;
 int main () {
     pid_t pid_value;
     int count = 0;
 
     for (int i = 0; i < 5; i++) {
+        /**
+        *@brief   getpid - get process identification
+        *@details returns the process ID of the calling process
+        *@return  process ID that is a pid_t types
+        *@sa      fork()
+        **/
         cout << "Parent process " << getpid()
              << ": Creating child" << endl;
 
+        /**
+        *@brief   fork - create a child process
+        *@details creates a new process by duplicating the calling process.
+                  The new process is referred to as the child process. The
+                  calling process is referred to as the parent process. The
+                  child process and the parent process run in separate memory
+                  spaces. At the time of fork() both memory spaces have the
+                  same content.
+        *@return  on success, the PID of the child process is returned in the
+                  parent, and 0 is returned in the child. On failure, -1 is
+                  returned in the parent, no child process is created, and
+                  errno is set appropriately.
+        **/
         pid_value = fork();
 
         /* If there was an error on duplication then */
@@ -27,6 +55,15 @@ int main () {
         if (pid_value == 0) {
             cout << "Child process " << getpid() << ": Running" << endl;
             count++;
+            /**
+            *@brief   sleep - sleep for the specified number of seconds
+            *@details makes the calling thread sleep until seconds have
+                      elapsed or a signal arrives which is not ignored.
+            *@param   unsigned int seconds - seconds that the thread will sleep
+            *@return  zero if the requested time has elapsed, or the number of
+                      seconds left to sleep, if the call was interrupted by a
+                      signal handler.
+            **/
             sleep(1);
             cout << "Child process " << getpid()
                  << ": Exiting with status " << count << endl;
@@ -40,17 +77,56 @@ int main () {
     int errno, status, sum = 0;
 
     for (int j = 0; j < 5; j++) {
-        /* Parent-process waits for all children to exit, adding each status to the sum variable */
+        /* Parent-process waits for all children to exit, adding each status
+           to the sum variable */
+        /**
+        *@brief   waitpid - wait for process to change state
+        *@details suspends execution of the calling process until a child
+                  specified by pid argument has changed state. By default,
+                  waitpid() waits only for terminated children.
+        *@param   pid_t pid - pid that will be waited by calling process. -1
+                  meaning wait for any child process.
+        *@param   int *status - if is not NULL, store status information in
+                  the int to which it points. This integer can be inspected with macros (which take the integer itself as an argument,
+                  not a pointer to it)
+        *@param   int options - some constants or zero that change behaviour of
+                  the function
+        *@return  on success, returns the process ID of the child whose state
+                  has changed. On error, -1 is returned.
+        *@sa      fork(), ECHILD, WIFEXITED(), WEXITSTATUS
+        **/
         waitpid(-1, &status, 0);
 
-        if (WIFEXITED(status))
-            sum += WEXITSTATUS(status);
-
+        /**
+        *@brief   ECHILD - error constant of waitpid procedure
+        *@details (for  waitpid()) the process specified by pid (waitpid())
+                  or idtype does not exist or is not a child of the calling
+                  process
+        **/
         if (errno == ECHILD) {
             cout << "Error on waitpid" << endl;
             return -1;
         }
 
+        /**
+        *@brief   WIFEXITED - macro of waitpid procedure
+        *@details returns true if the child terminated normally, that is, by
+                  calling exit(3) or _exit(2), or by returning from main().
+        *@param   int status - status of process
+        *@return  boolean type
+        **/
+        if (WIFEXITED(status))
+            /**
+            *@brief   WEXITSTATUS - macro of waitpid procedure
+            *@details returns the exit status of the child. This consists of
+                      the least significant 8 bits of the status argument that
+                      the child specified in a call to exit(3) or _exit(2) or
+                      as the argument for a return statement in main(). This
+                      macro should be employed only if WIFEXITED returned true.
+            *@param   int status - status of process
+            *@return  int type that is the status
+            **/
+            sum += WEXITSTATUS(status);
     }
 
     cout << "Parent process " << getpid()
